feb4353b1706d36401a314693c7b32bd924f16e6
nasa <htilcs1115@gmail.com>
2019-02-18 08:55:37 +0900

    feat: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+use rocket::fairing::AdHoc;
+use rocket::request::*;
+use rocket::response::*;
+use rocket_contrib::serve::StaticFiles;
+use std::path::{Path, PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
