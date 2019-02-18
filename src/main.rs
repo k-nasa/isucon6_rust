a2ca2fa5519d8b6c17941cf64c6aee97f58ae6f0
nasa <htilcs1115@gmail.com>
2019-02-18 09:07:43 +0900

    feat: /initialize

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,34 @@
 
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate mysql;
+
 use rocket::fairing::AdHoc;
-use rocket::request::*;
-use rocket::response::*;
+// use rocket::request::;
+use rocket::response::content;
 use rocket_contrib::serve::StaticFiles;
 use std::path::{Path, PathBuf};
 
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
+}
+
+fn dbh() -> mysql::Pool {
+    mysql::Pool::new("mysql://root:password@localhost:3307/isuda").unwrap()
+}
+
+#[get("/initialize")]
+fn initialize() -> content::Json<&'static str> {
+    init_query().unwrap();
+    content::Json("{ 'result': 'ok' }")
+}
+
+fn init_query() -> Result<(), mysql::Error> {
+    let pool = dbh();
+    pool.prep_exec("DELETE FROM entry where id > 7101", ())?;
+    pool.prep_exec("TRUNCATE star", ())?;
+    Ok(())
 }
 
 #[get("/")]
